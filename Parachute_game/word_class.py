de576a509a347f9e3d526cc67e29eb42f8ec73de
jordankraude <jordankraudeTP@gmail.com>
2022-02-07 09:57:14 -0700

    updates

--- Parachute_game/word_class.py
+++ Parachute_game/word_class.py
@@ -10,7 +10,7 @@ class Word:
 
     def _getword(self):
         self._word = random.choice(self._list)
-        self._word_final = self.word.replace('\n', '')
+        self._word_final = self._word.replace('\n', '')
         self._word_as_list = []
         for char in self._word_final:
             char.append(self._word_as_list)
